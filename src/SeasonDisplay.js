34526a88fa0aed09717f2aac184fcb7191935211
tutkuince <tutku.ince@outlook.com>
2019-12-04 23:18:28 +0300

    initial commit

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SeasonDisplay extends React.Component {
+    render() {
+        return (
+            <div>Season Display</div>
+        );
+    }
+}
+
+export default SeasonDisplay;
