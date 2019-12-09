8c1f81adbd7c61f44f070a64e8537dde6aa2e782
tutkuince <tutku.ince@outlook.com>
2019-12-09 21:15:31 +0300

    constructor is created and state object invoked and getCurrentPosition method set into constructor

--- src/index.js
+++ src/index.js
@@ -2,15 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render() {
+    // State must be initialized when a component is created. (constructor)
+    constructor(props) {
+        super(props);
+
+        // This is the only time we do direct assignment to this.state
+        this.state = {
+            lat: null
+        };
+
         // Getting the current position
         window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
+            position => {
+                this.setState({lat: position.coords.latitude});
+            },
             positionError => console.log(positionError)
         );
+    }
 
+    // React says we have to define render.
+    render() {
         return (
-            <h2>Latitude: </h2>
+            <h2>Latitude: {this.state.lat}</h2>
         );
     }
 }
