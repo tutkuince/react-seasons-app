3eaa2853680e8e62a9da7800bd4ef1f39412ef67
tutkuince <tutku.ince@outlook.com>
2019-12-11 19:41:24 +0300

    created getSeason function

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-class SeasonDisplay extends React.Component {
-    render() {
-        return (
-            <div>Season Display</div>
-        );
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+
+    return <div>Season Display</div>;
+}
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
     }
 }
 
