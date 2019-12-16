d32892e9364e80cba64f8b3211b966c7edd187bc
tutkuince <tutku.ince@outlook.com>
2019-12-16 20:48:43 +0300

    Spinner used with default props or override default props

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const Spinner = () => {
+const Spinner = (props) => {
     return (
         <div className="ui active dimmer">
-            <div className="ui text loader">Loading...</div>
+            <div className="ui text loader">{props.message}</div>
         </div>
     );
+};
+
+// Default props define like this way.
+Spinner.defaultProps = {
+  message: 'Loading...'
 };
 
 export default Spinner;
