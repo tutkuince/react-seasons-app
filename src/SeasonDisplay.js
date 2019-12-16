7a86a55e628681380d72bb285421f4f8312cbfd4
tutkuince <tutku.ince@outlook.com>
2019-12-16 18:54:57 +0300

    icon size is got bigger with massive class name.

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -17,9 +17,9 @@ const SeasonDisplay = (props) => {
 
     return (
         <div>
-            <i className={`${iconName} icon`} />
+            <i className={`massive ${iconName} icon`} />
             <h1>{text}</h1>
-            <i className={`${iconName} icon`} />
+            <i className={`massive ${iconName} icon`} />
         </div>
     );
 }
