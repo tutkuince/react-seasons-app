4e42cec3c152495c67e55a77d385e7c1e776e37b
tutkuince <tutku.ince@outlook.com>
2019-12-16 21:06:40 +0300

    renderContent

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -3,6 +3,19 @@ import './SeasonDisplay.css';
 
 import React from 'react';
 
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const {text, iconName} = seasonConfig[season]; // {text, iconName}
+
+    return (
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`}/>
+            <h1>{text}</h1>
+            <i className={`icon-right massive ${iconName} icon`}/>
+        </div>
+    );
+};
+
 const seasonConfig = {
     summer: {
         text: "Let\'s hit the beach!",
@@ -14,25 +27,12 @@ const seasonConfig = {
     }
 };
 
-const SeasonDisplay = (props) => {
-    const season = getSeason(props.lat, new Date().getMonth());
-    const {text, iconName } = seasonConfig[season]; // {text, iconName}
-
-    return (
-        <div className={`season-display ${season}`}>
-            <i className={`icon-left massive ${iconName} icon`} />
-            <h1>{text}</h1>
-            <i className={`icon-right massive ${iconName} icon`} />
-        </div>
-    );
-}
-
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9) {
         return lat > 0 ? 'summer' : 'winter';
     } else {
         return lat > 0 ? 'winter' : 'summer';
     }
-}
+};
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -16,8 +16,7 @@ class App extends React.Component {
         );
     }
 
-    // React says we have to define render.
-    render() {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
@@ -27,6 +26,15 @@ class App extends React.Component {
         }
 
         return <Spinner message="Please accept location request!"/>;
+    }
+
+    // React says we have to define render.
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
     }
 }
 
